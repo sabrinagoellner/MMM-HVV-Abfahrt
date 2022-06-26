b92f55c9f3e2b9f60e848f46841ccfe97fb61cc1
Sabrina Göllner <goellnersabrina@gmail.com>
2022-06-26 19:31:16 +0200

    using firefox

--- node_helper.js
+++ node_helper.js
@@ -29,7 +29,7 @@ module.exports = NodeHelper.create({
     
 
     scrapeURL: async function (url) {
-        const browser = await playwright.chromium.launch({
+        const browser = await playwright.firefox.launch({
             headless: true // setting this to true will not run the UI
         });
 
